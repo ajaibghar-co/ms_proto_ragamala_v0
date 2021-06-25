0a59359b37bbfe28552503dfe5ba445abc0187fa
ambikajo <ambikajoshi@gmail.com>
2021-06-25 22:25:09 +0530

    added the information from the google doc about ragamala painting and other information in dict.json, changed the button color

--- sketch.js
+++ sketch.js
@@ -1,62 +1,187 @@
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/U7b1I0N-1/';
 
-  // Classifier Variable
-  let classifier;
-  // Model URL
-  let imageModelURL = 'https://teachablemachine.withgoogle.com/models/U7b1I0N-1/';
-  
-  // Video
-  let video;
-  let flippedVideo;
-  // To store the classification
-  let label = "";
+//details for hte classifier
+let label = "";
+let conf;
+let confArr = [];
+// let confArr2;
+//make a variable for a changing div
+let awrd;
+//keywords for the different knots
+let k1;
+let k2;
+let k3;
+let k4;
+let k5;
 
-  // Load the model first
-  function preload() {
-    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+//basic requirements
+let button; //accept button
+let answerbtn;
+let intro;
+let constraints;
+let machAns;
+let machAns2;
+let answerfinal;
+let diff;
+let bk;
+//video variable
+let vid;
+
+//dictionary for the qna
+let dict;
+
+
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  dict = loadJSON("dict.json")
+}
+
+function setup() {
+  // createCanvas(200, 200);
+  // background(200)
+  noCanvas()
+  constraints = {
+    video: {
+
+      facingMode: {
+        exact: "environment"
+      }
+
+    },
+    audio: false
+  };
+
+
+
+  textFont('Rajdhani')
+  button = createButton("Accept")
+  button.addClass("btnclass")
+  button.mousePressed(startStream)
+
+  awrd = createDiv("")
+  awrd.addClass("awrd")
+
+  machAns = createDiv("")
+  machAns.addClass("rAns")
+
+  machAns2 = createP("")
+  machAns2.parent(machAns)
+  machAns2.addClass("spanAns")
+
+
+  bk = createButton("Back")
+  bk.addClass("back")
+  bk.hide()
+  bk.mousePressed(moveBG)
+
+
+  k1 = "music"
+  k2 = "inscriptions"
+  k3 = "ragamala"
+  k4 = "jharokha"
+
+  a1 = dict.trigger.one.desc
+  a2 = dict.trigger.two.desc
+  a3 = dict.trigger.three.desc
+  a4 = dict.trigger.four.desc
+
+  answerbtn = createButton("Know more")
+
+  answerbtn.hide()
+  answerbtn.id("ansB")
+}
+//check orientation rather than platform
+function startStream() {
+
+  if (navigator.platform == "iPhone" || navigator.platform == "Linux armv8l") {
+    vid = createCapture(VIDEO, constraints)
+  } else {
+
+    vid = createCapture(VIDEO);
   }
 
-  function setup() {
-    createCanvas(320, 260);
-    // Create the video
-    video = createCapture(VIDEO);
-    video.size(320, 240);
-    video.hide();
+  vid.position(0, 0)
+  button.remove()
+  select("#accept").remove()
+  classifyVideo()
 
-    flippedVideo = ml5.flipImage(video);
-    // Start classifying
-    classifyVideo();
+
+}
+// Get a prediction for the current video frame
+function classifyVideo() {
+  classifier.classify(vid, gotResult);
+
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
   }
+  // The results are in an array ordered by confidence.
 
-  function draw() {
-    background(0);
-    // Draw the video
-    image(flippedVideo, 0, 0);
+  label = results[0].label;
+  conf = results[0].confidence
+  // Classify again!
+  classifyVideo();
+  answerbtn.show()
+}
 
-    // Draw the label
-    fill(255);
-    textSize(16);
-    textAlign(CENTER);
-    text(label, width / 2, height - 4);
-  }
+function draw() {
 
-  // Get a prediction for the current video frame
-  function classifyVideo() {
-    flippedVideo = ml5.flipImage(video)
-    classifier.classify(flippedVideo, gotResult);
-    flippedVideo.remove();
+  addFrm()
 
-  }
+}
 
-  // When we get a result
-  function gotResult(error, results) {
-    // If there is an error
-    if (error) {
-      console.error(error);
-      return;
+function addFrm() {
+  if (conf > 0.8) {
+    if (label == "Music") {
+      cArr(k1, a1)
     }
-    // The results are in an array ordered by confidence.
-    // console.log(results[0]);
-    label = results[0].label;
-    // Classifiy again!
-    classifyVideo();
+    if (label == "Inscriptions") {
+      cArr(k2, a2)
+    }
+    if (label == "Ragamala") {
+      cArr(k3, a3)
+    }
+    if (label == "Jharokha") {
+      cArr(k4, a4)
+    }
+   
+
   }
+}
+
+
+function giveanswer() {
+  answerbtn.hide()
+  machAns.show()
+  machAns2.html(answerfinal)
+
+  //when you click generate answer, the generated answer will synthesis as audio
+
+  confArr = []
+  if (machAns2 != "") {
+    bk.show()
+  }
+}
+
+function cArr(k, i) {
+  awrd.html(k);
+  answerbtn.mousePressed(giveanswer)
+  answerfinal = i
+
+}
+
+function moveBG() {
+  //if you click back button the audio will stop
+
+  machAns.hide()
+  bk.hide()
+}
+
